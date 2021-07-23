964b9d690c26213c716ac5e367592fa8e51fff73
mk1121 <marufm.3666.com>
2021-07-23 12:27:45 +0600

    add logo,course api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect, useState } from "react";
 function App() {
+  const [course, setCourse] = useState([]);
+  console.log(course);
+  useEffect(() => {
+    fetch("data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((dat) => {
+        console.log(dat);
+        setCourse(dat);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo bg-primary">
+        <img src="logo3.png" alt="logo" />
+      </div>
+
+      {course.map((d) => (
+        <div className="course-section">
+          <p>{d.title}</p>
+          <img src={d.imgUrl} alt={d.title} />
+        </div>
+      ))}
     </div>
   );
 }
